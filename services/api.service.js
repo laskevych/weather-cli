546d330a0d26a1b272458991f047daa67486f705
Andrew Laskvych <a.laskevych@owox.com>
2022-08-28 17:28:36 +0300

    Add logic of saving city

--- services/api.service.js
+++ services/api.service.js
@@ -2,13 +2,17 @@ import axios from "axios";
 import { getValueByKey, STORAGE_KEYS } from './storage.service.js';
 
 
-const getWeather = async (city) => {
+const getWeather = async () => {
     const token = await getValueByKey(STORAGE_KEYS.token);
 
     if (!token) {
         throw new Error('Please save token by flag -t.');
     }
 
+    const city = await getValueByKey(STORAGE_KEYS.city);
+    if (!city) {
+        throw new Error('Please save city by flag -s.');
+    }
 
     const { data } = await axios.get('https://api.openweathermap.org/data/2.5/weather', {
         params: {
--- weather.js
+++ weather.js
@@ -3,8 +3,9 @@
 import { getArgs } from './helpers/args.js';
 import { getWeather } from './services/api.service.js';
 import { printError, printHelp, printSuccess } from './services/log.service.js';
-import { saveValueByKey, STORAGE_KEYS } from './services/storage.service.js'
+import { getValueByKey, saveValueByKey, STORAGE_KEYS } from './services/storage.service.js'
 //ebb3f7ebf60cb5a08340c51c9b19d8c1
+
 const saveToken = async (token) => {
     try {
         await saveValueByKey(STORAGE_KEYS.token, token);
@@ -12,6 +13,34 @@ const saveToken = async (token) => {
         printSuccess('Token was saved.');
     } catch (error) {
         printError(`Token not saved. Details: ${error.message}`);
+    }
+};
+
+const saveCity = async (city) => {
+    try {
+        await saveValueByKey(STORAGE_KEYS.city, city);
+
+        printSuccess('City was saved.');
+    } catch (error) {
+        printError(`City not saved. Details: ${error.message}`);
+    }
+};
+
+const getForcast = async () => {
+    try {
+        const weather = await getWeather();
+        console.log(weather);
+    } catch (error) {
+        let errorMessage;
+        if (error?.response?.status === 404) {
+            errorMessage = `Incorrect city ${getValueByKey(STORAGE_KEYS.city)}`;
+        } else if (error?.response?.status === 401) {
+            errorMessage = `Incorrect token ${getValueByKey(STORAGE_KEYS.token)}`;
+        } else {
+            errorMessage = error.message;
+        }
+
+        printError(errorMessage);
     }
 };
 
@@ -25,14 +54,14 @@ const initCLI = () => {
     }
 
     if (args.s) {
-        //todo save city
-    } 
+        saveCity(args.s);
+    }
         
     if (args.t) {
         saveToken(args.t);
     }
 
-    getWeather('Kyiv');
+    getForcast()
 }
 
 initCLI();
